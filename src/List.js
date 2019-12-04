2c78547b7f10ff3562b4e9b232daf87b482f0bcc
AndriyKor <andriy.korovetskyy@gmail.com>
2019-12-04 21:45:16 +0200

    Added List control

---
+++ src/List.js
@@ -0,0 +1,35 @@
+import React from "react";
+
+function List(props) {
+  return props.items.length > 0 ? (
+    <table className="table table-striped">
+      <thead>
+        <tr>
+          <th scope="col">#</th>
+          <th scope="col">Item Name</th>
+          <th scope="col"></th>
+        </tr>
+      </thead>
+      <tbody>
+        {props.items.map(item => (
+          <tr key={item}>
+            <th>{props.items.indexOf(item) + 1}</th>
+            <td>{item}</td>
+            <td className="text-right">
+              <button
+                className="btn btn-danger"
+                onClick={() => props.handleDelete(item)}
+              >
+                Delete
+              </button>
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  ) : (
+    <h4>The list is empty</h4>
+  );
+}
+
+export default List;
