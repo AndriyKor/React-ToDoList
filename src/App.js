7e3dd9fb869527a4e8cff07a54a193a6edfb83f7
AndriyKor <andriy.korovetskyy@gmail.com>
2019-12-04 21:47:11 +0200

    Added implementation in App control

--- src/App.js
+++ src/App.js
@@ -1,26 +1,86 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+//import "./App.css";
+import List from "./List";
+import "bootstrap/dist/css/bootstrap.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: JSON.parse(localStorage.getItem("ListItems")) || [],
+      value: ""
+    };
+    console.log(this.state);
+  }
+
+  handleChange = event => {
+    this.setState({
+      value: event.target.value
+    });
+  };
+
+  handleClick = () => {
+    const { items, value } = this.state;
+
+    if (items.includes(value)) {
+      alert(`Item '${value}' is already exist`);
+    } else {
+      const newItems = items.concat(value);
+
+      this.storeItems(newItems);
+
+      this.setState({
+        items: newItems,
+        value: ""
+      });
+    }
+  };
+
+  handleDelete = item => {
+    const newItems = this.state.items.filter(value => {
+      return value !== item;
+    });
+
+    this.storeItems(newItems);
+
+    this.setState({
+      items: newItems
+    });
+  };
+
+  storeItems(items) {
+    localStorage.setItem("ListItems", JSON.stringify(items));
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <h1>This is ToDo list</h1>
+        <div className="row">
+          <div className="col">
+            <div className="input-group mb-3">
+              <input
+                className="form-control"
+                placeholder="Enter item here"
+                value={this.state.value}
+                onChange={this.handleChange}
+              />
+              <div className="input-group-append">
+                <button
+                  className="btn btn-outline-secondary"
+                  type="button"
+                  onClick={this.handleClick}
+                >
+                  Add Item
+                </button>
+              </div>
+            </div>
+            <List items={this.state.items} handleDelete={this.handleDelete} />
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
